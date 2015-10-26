9a6d996729a3960e1ff4977274ea6a20dc14cae4
Jon Lowrey <Jon@minerva.local>
2015-10-25 19:17:52 -0500

    Defined random QS

--- PA2/rand_quick_sort.h
+++ PA2/rand_quick_sort.h
@@ -10,30 +10,34 @@
 #define rand_quick_sort_h
 
 
-int partition (int arr[], int l, int h)
+int random_partition(int* arr, int start, int end)
 {
-    int x = arr[h];
-    int i = (l - 1);
+    srand(time(NULL));
+    int pivotIdx = start + rand() % (end-start+1);
+    int pivot = arr[pivotIdx];
+    swap(arr[pivotIdx], arr[end]); // move pivot element to the end
+    pivotIdx = end;
+    int i = start -1;
     
-    for (int j = l; j <= h- 1; j++)
+    for(int j=start; j<=end-1; j++)
     {
-        if (arr[j] <= x)
+        if(arr[j] <= pivot)
         {
-            i++;
-            swap (&arr[i], &arr[j]);
+            i = i+1;
+            swap(arr[i], arr[j]);
         }
     }
-    swap (&arr[i + 1], &arr[h]);
-    return (i + 1);
-}
-void quickSort(int A[], int l, int h)
-{
-    if (l < h)
-    {
-        int p = partition(A, l, h); /* Partitioning index */
-        quickSort(A, l, p - 1);
-        quickSort(A, p + 1, h);
-    }
+    
+    swap(arr[i+1], arr[pivotIdx]);
+    return i+1;
 }
 
+void random_quick_sort(int* arr, int start, int end)
+{
+    if(start < end) {
+        int mid = random_partition(arr, start, end);
+        random_quick_sort(arr, start, mid-1);
+        random_quick_sort(arr, mid+1, end);
+    }
+}
 #endif /* rand_quick_sort_h */
