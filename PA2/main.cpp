194affb6be957d9a3802944ab78105a58054cc80
Jon Lowrey <Jon@minerva.local>
2015-10-28 16:04:02 -0500

    Finished

--- PA2/main.cpp
+++ PA2/main.cpp
@@ -5,7 +5,6 @@
 #include <algorithm>
 #include "quick_sort.h"
 #include "insertion_sort.h"
-#include "merge_sort.h"
 #include "util.h"
 
 using namespace std;
@@ -27,7 +26,12 @@ void printArray (int arr[],int size)
         cout<<j<<": "<<arr[j]<<endl;
     }
 }
-
+void partialSort(int* arr, int sizeOfArray)
+{
+    quickSort(arr, 0, sizeOfArray);
+    swap(&arr[0],&arr[sizeOfArray-1]);
+    
+}
 
 
 
@@ -37,39 +41,30 @@ int main(int argc, const char * argv[]) {
     int elements;
     cout<<"How many elements in the array?"<<endl;
     cin>>elements;
-    //for(int apple = 0;apple<1000;apple++){
+    for(int apple = 0;apple<1000;apple++){
     int j[elements];
     randomArray(j, elements);
-    int k[elements];//array to be quicksorted
+    int k [elements];//Array to be partially sorted then insertion sorted.
     int l[elements];//array to be insertion sorted
     for(int i=0;i<elements;i++)
-    {
-        k[i]=j[i];
+    {   k[i]=j[i];
         l[i]=j[i];
     }
-      //  cout<<endl<<"--------------------------------------------"<<endl;
-  /*
-    //Portion 1 Quicksort
+    partialSort(k,elements);
+
+    //Portion 1 Insertion Sort
     std::chrono::high_resolution_clock::time_point time1 = std::chrono::high_resolution_clock::now();
-    quickSort(k, 0, elements-1);
-    std::chrono::high_resolution_clock::time_point time2 = std::chrono::high_resolution_clock::now();
-    long long  durationQ = std::chrono::duration_cast<std::chrono::microseconds>( time2 - time1 ).count();
-    //cout<<endl<<"Running Time for Quicksort is: "<<durationQ<<endl;
-    //cout<<"SORTED"<<endl;
-    //for(int i = 0 ;i<elements;i++){cout<<i<<": "<<k[i]<<endl;}//PRINT STATEMENT DEBUG
-    //cout<<endl<<"--------------------------------------------"<<endl;
-    //Portion 2 Insertion Sort
-    std::chrono::high_resolution_clock::time_point time3 = std::chrono::high_resolution_clock::now();
     insertionSort(l, elements);
+    std::chrono::high_resolution_clock::time_point time2 = std::chrono::high_resolution_clock::now();
+    long long  durationI = std::chrono::duration_cast<std::chrono::microseconds>( time2 - time1 ).count();
+    //Partially Sorted Portion
+    std::chrono::high_resolution_clock::time_point time3 = std::chrono::high_resolution_clock::now();
+    insertionSort(k, elements);
     std::chrono::high_resolution_clock::time_point time4 = std::chrono::high_resolution_clock::now();
-    long long  durationI = std::chrono::duration_cast<std::chrono::microseconds>( time4 - time3 ).count();
-    //cout<<endl<<"Running Time for Insertion Sort is: "<<durationI<<endl;
-    cout<<durationQ<<" "<<durationI<<endl;
-    //for(int i = 0 ;i<elements;i++){cout<<i<<": "<<l[i]<<endl;}//PRINT STATEMENT DEBUG
+    long long  durationIR = std::chrono::duration_cast<std::chrono::microseconds>( time4 - time3 ).count();
+    cout<<durationI<<" "<<durationIR<<endl;
     }
-    */
-        insertionSort(k,elements);
-        printArray(k,elements);
+
     
 }
 
